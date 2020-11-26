75faae62f59d82565b2a62507fcfd436d0f5f834
idhant96 <idhant123@gmail.com>
2020-11-26 13:32:57 -0700

    basic api made

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,32 @@
 from flask import Flask
 import load_data
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello Idhant."
+	d = {'head': {'vars': ['object1', 'object2', 'object3']}, 'results': {'bindings': [{'object1': {'type': 'uri', 'value': 'http://www.semanticweb.org/swarnalatha/ontologies/2020/10/untitled-ontology-28#Cronus'}, 'object2': {'type': 'uri', 'value': 'http://www.semanticweb.org/swarnalatha/ontologies/2020/10/untitled-ontology-28#Rhea'}}]}}
+	return load_data.clean_data(d)
 
 @app.route('/character/<character>')
 def show_dropdown_list(character):
-    return load_data.form_query(character)
+    query = load_data.form_query(character)
+    data = load_data.load_data(query)
+    data = load_data.clean_data(data)
+    return data
+
+@app.route('/show/')
+def show_result():
+	character = request.args.get('character')
+	filters = []
+	vars = []
+	filters.append(request.args.get('filter1'))
+	filters.append(request.args.get('filter2'))
+	vars.append(request.args.get('var1'))
+	vars.append(request.args.get('var2'))
+	vars.append(request.args.get('var3'))
+	query = load_data.form_query2(character, filters, vars)
+	data = load_data.load_data(query)
+	data = load_data.clean_data(data)
+	return data
--- backend/load_data.py
+++ backend/load_data.py
@@ -4,20 +4,35 @@ from SPARQLWrapper import SPARQLWrapper, JSON, XML
 
 sparql = SPARQLWrapper("http://18.144.52.83:3030/ser531")
 
+
 prefix_template = "<http://www.semanticweb.org/swarnalatha/ontologies/2020/10/untitled-ontology-28#{0}>"
+
+clause_template = '''
+
+  {subject} {predicate} {object} .
+
+'''
 
 main_template = """
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-SELECT ?subject ?predicate ?object
-WHERE {{
-  {subject} {predicate} ?object .
-}}
+SELECT {var1} {var2} {var3}
+WHERE {{ {where_clause} }}
 
 """
 
+def clean_data(data={}):
+	vars = data['head']['vars']
+	bindings = data['results']['bindings']
+
+	for binding in bindings:
+		for var in vars:
+			if var in binding:
+				if binding[var]['type'] == 'uri':
+					binding[var]['value'] = binding[var]['value'].split('#')[1]
+	return data
 
 
 
@@ -25,11 +40,17 @@ def load_data(query):
 	sparql.setQuery(query)
 	sparql.setReturnFormat(JSON)
 	results = sparql.query().convert()
-	print (json.dumps(results))
+	return results
 
 def form_query(character, filter="?predicate"):
-	prefix_str = prefix_template.format(character)
-	query = template.format(subject=prefix_str, predicate=filter)
-	load_data(query)
-	print(query)
-	return "somehting"
+	prefix_subject = prefix_template.format(character)
+	query = main_template.format(where_clause=clause_template.format(subject=prefix_subject, predicate=filter, object="?object"), var1="?subject", var2="?predicate", var3="?object")
+	return query
+
+def form_query2(character, filters=[], vars=[]):
+	where = []
+	prefix_subject = prefix_template.format(character)
+	for i in range(len(filters)):
+		where.append(clause_template.format(subject=prefix_subject,object=vars[i] ,predicate=prefix_template.format(filters[i])))
+	query = main_template.format(where_clause=''.join(where),var1=vars[0], var2=vars[1], var3=vars[2] )
+	return query
